662cd2b53699d28da33431d362ec3638d02a4713
saim-raza-ai-rabotic <saimraza013q@gmail.com>
2025-08-30 09:34:20 +0500

    Create calculator_input_int.py

---
+++ calculator_input_int.py
@@ -0,0 +1,13 @@
+Interactive Python calculator user input (with int support)
+
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("Addition       :", (a) + (b))
+print("Subtraction    :", (a) - (b))
+print("Multiplication :", (a) * (b))
+print("Division       :", (a) / (b))
+print("Floor Division :", (a) // (b))
+print("Modulus        :", (a) % (b))
+print("Exponent       :", (a) ** (b))
