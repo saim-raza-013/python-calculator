29c3024d351f7872feb77fe8e9a9ec1940b9ad1c
saim-raza-ai-rabotic <saimraza013q@gmail.com>
2025-08-30 09:37:13 +0500

    Create calculator_input_float.py

---
+++ calculator_input_float.py
@@ -0,0 +1,12 @@
+Interactive Python  calculator user input  ( int and float support)
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+print("Addition       :", a + b)
+print("Subtraction    :", a - b)
+print("Multiplication :", a * b)
+print("Division       :", a / b)
+print("Floor Division :", a // b)
+print("Modulus        :", a % b)
+print("Exponent       :", a ** b)
